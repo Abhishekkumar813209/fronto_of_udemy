49a39cfebb0469dec68aeb2d4989fe6a1d403c47
Bibash Roy <bibhash@gmail.com>
2023-07-14 17:23:26 +0530

    completed 15minutes

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { ColorModeSwitcher } from "../../../ColorModeSwitcher"
-import { Button,Drawer, DrawerOverlay,DrawerContent,DrawerHeader, DrawerBody, useDisclosure} from '@chakra-ui/react'
+import { Button,Drawer, DrawerOverlay,DrawerContent,DrawerHeader, DrawerBody, useDisclosure,VStack,HStack} from '@chakra-ui/react'
 import {RiMenu5Fill} from "react-icons/ri";
 import { Link } from 'react-router-dom';
+import { isAuthenticated } from '../../../../../backend/middlewares/auth';
 
-const funtion LinkButton = (url="/",title="Home") =>(
+const LinkButton = ({url="/",title="Home"}) =>(
     <Link to={url}>
         <Button variant={'ghost'}> {title} </Button>
     </Link>
@@ -34,13 +35,32 @@ const Header = () => {
                 <DrawerOverlay />
                     <DrawerContent>
                         <DrawerHeader borderBottomWidth={'1px'}> Course Bundler </DrawerHeader>
-                        <DrawerBody> </DrawerBody>
-                    </DrawerContent>
-            </Drawer>
+                        <DrawerBody> 
+                        
+                <VStack spacing={'4'} alignItems="flex-start">
+                    <LinkButton url="/" title="Home" />
+                    <LinkButton url="/courses" title="Browse All Courses" />
+                    <LinkButton url="/request" title="Request a Course" />
+                    <LinkButton url="/contact" title="Contact Us" />
+                    <LinkButton url="/about" title="About" />
+                </VStack>
+
+                <HStack justifyContent={"space-evenly"} position="absolute" bottom={"2 rem"} width="80%">
+                    {
+                        isAuthenticated?(<></>):(
+                            <>
+                                <Link to="/login">
+                                    <Button>Login</Button>
+                                </Link>
+                            </>
+                        )
+                    }
+                </HStack>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
             
-    <VStack>
             
-    </VStack>
         </>
     )
 }
