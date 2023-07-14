80d4aa27d4b20f1a58ea1aded75a67193480db4b
Bibash Roy <bibhash@gmail.com>
2023-07-14 15:54:02 +0530

    adding drawer on website

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { ColorModeSwitcher } from "../../../ColorModeSwitcher"
+import { Button,Drawer} from '@chakra-ui/react'
+import {RiMenu5Fill} from "react-icons/ri";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+        <>
+            <ColorModeSwitcher />
+            <Button 
+            colorSchema={"yellow"} 
+            width="12" 
+            height={'12'} 
+            rounded='full'
+            position={'fixed'}
+            top="6"
+            left="6"
+            >
+                <RiMenu5Fill />
+            </Button>
+
+            <Drawer>
+
+            </Drawer>
+
+        </>
+
+    )
 }
 
 export default Header
