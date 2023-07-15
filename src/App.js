a794c5c2fb0de720a2b2276386cff1ff8e70ed83
Bibash Roy <bibhash@gmail.com>
2023-07-15 13:59:23 +0530

    added signup page with upload file functionality see last 2 minutes video

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses'
 import Footer from './components/Layout/Footer/Footer';
 import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
 
 function App() {
   return (
      <Router>    
        <Header />
+       <div style={{ marginBottom: '60px' }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
+      </div>
         <Footer />
-
 
 
      </Router>
