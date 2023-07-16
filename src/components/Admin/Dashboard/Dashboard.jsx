4ec73d0cdb456e78b5dc517596acc0a14088d008
Bibash Roy <bibhash@gmail.com>
2023-07-16 19:44:19 +0530

    completed admin dashboard till 14 but still views graph column is sidebar aligned it should be aligned below databox now charts and graphs will be made

--- src/components/Admin/Dashboard/Dashboard.jsx
+++ src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,34 +1,42 @@
-import { Box, Grid ,Heading,Stack,Text, HStack} from '@chakra-ui/react'
+import { Box, Grid ,Heading,Stack,Text, HStack,Progress} from '@chakra-ui/react'
 import React from 'react'
 import Sidebar from '../Sidebar'
 import { RiArrowDownLine, RiArrowUpLine } from 'react-icons/ri'
 
-const Dashboard = ({title,qty,qtyPercentage,profit}) => {
-
-    <Box 
-    w={['full','20%']} 
-    boxShadow={'-2px 0 10px rgba(107,70,190,0.5)'}
-    p="8"
-    borderRadius={'lg'}
-    >
-    
-    <Text children = {title} />
-
-       <HStack spacing={'6'}>
-        <Text fontSize={'2xl'} fontWeight="bold" children={qty} />
-        <HStack>
+const Databox = ({ title, qty, qtyPercentage, profit }) => {
+    return (
+      <Box
+        w={['full', '20%']}
+        boxShadow={'-2px 0 10px rgba(107,70,190,0.5)'}
+        p="8"
+        borderRadius={'lg'}
+      >
+        <Text children={title} />
+  
+        <HStack spacing={'6'}>
+          <Text fontSize={'2xl'} fontWeight="bold" children={qty} />
+          <HStack>
             <Text children={`${qtyPercentage}%`} />
-            {profit ? (
-                <RiArrowUpLine color = "green" />
-            ):(
-                <RiArrowDownLine color="red" />
-            )}
+            {profit ? <RiArrowUpLine color="green" /> : <RiArrowDownLine color="red" />}
+          </HStack>
         </HStack>
-       </HStack>
-        <Text opacity={0.6} children={'Since Last Month'} /> 
-     </Box>
+        <Text opacity={0.6} children={'Since Last Month'} />
+      </Box>
+    );
+    }
 
-    
+    const Bar = ({title,value,profit}) =>(
+        <Box py="4" px={['0','20']}>
+            <Heading size="sm" children={title} mb='2' />
+            <HStack w="full" alignItems={'center'}>
+                <Text children={profit?"0%": `-${value}%` } />
+                <Progress w="full" value={profit?value:0} colorScheme="purple" />
+                <Text children={`${value >100? value:100}%`} />
+            </HStack>
+        </Box>
+    )
+
+const Dashboard = () => {    
 
   return (
    <Grid
@@ -75,13 +83,38 @@ const Dashboard = ({title,qty,qtyPercentage,profit}) => {
     boxShadow={'-2px 0 10px rgba(107,70,193,0.5)'}
     >
 
-    <Heading textAlign={['center','left']} size='md' children="Views Graph" pt={['8','0']} />
+    <Heading textAlign={['center','left']} size='md' children="Views Graph" pt={['8','0']} 
+        ml={['0','16']}
+    />
 
 
     </Box>
-    <Sidebar />
 
-    
+    {/* Line Graph Here */}
+    <Grid templateColumns={['1fr','2fr 1fr']}>
+        <Box p='4'>
+            <Heading
+            textAlign={['center','left']}
+            size='md'
+            children='Progress Bar'
+            my='8'
+            ml={['0','16']}
+            />
+            <Box>
+                <Bar profit={true} title= "Views" value={30} />
+                <Bar  profit={true}  title="Users" value={78} />
+                <Bar  profit={false} title="Subscription" value={20} />
+            </Box>
+        </Box>
+
+        <Box p={['0','16']} boxSizing='border-box' py='4'>
+            <Heading textAlign={'center'} size="md" mb="4" children="Users" />
+
+            {/* Donut graph */}
+        </Box>
+
+        </Grid>
+    <Sidebar />
    </Grid>
   )
 }
